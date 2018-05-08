3155857a824a8ad634d3ae01cc0bbd52bc536b86
Noah <njbotkin@gmail.com>
2018-05-08 04:30:43 -0500

    done what I can on TWA

---
+++ client/routes/calculate/generate-table.js
@@ -0,0 +1,8 @@
+import GenerateTable from './GenerateTable.html'
+
+export default () => ({
+	title: 'Calculate TWA',
+	name: `app.generate-table`,
+	route: `calculate/generate-table`,
+	template: GenerateTable
+})
---
+++ client/routes/calculate/twa.js
@@ -0,0 +1,22 @@
+import TWA from './TWA.html'
+
+export default () => ({
+	title: 'Calculate TWA',
+	name: `app.calculate-twa`,
+	route: `calculate/twa`,
+	template: TWA,
+	querystringParameters: [ `hours`, `samples` ],
+
+	resolve(data, params) {
+		var samplesArr = []
+		if(params.samples) {
+			for(; params.samples > 0; params.samples--) {
+				samplesArr.push(0)
+			}
+		}
+		return Promise.resolve({
+			hours: params.hours,
+			samples: samplesArr
+		})
+	},
+})
