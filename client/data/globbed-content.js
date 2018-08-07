d3cf15ebfe756808364c45d7eefac6aed71e1a1d
Noah <njbotkin@gmail.com>
2018-08-07 16:07:34 -0500

    add data to git

---
+++ client/data/globbed-content.js
@@ -0,0 +1,161 @@
+import client$47$data$47$content$47$1$46$html from '../../client/data/content/1.html'
+import client$47$data$47$content$47$10$45$1a$46$html from '../../client/data/content/10-1a.html'
+import client$47$data$47$content$47$10$45$1b$46$html from '../../client/data/content/10-1b.html'
+import client$47$data$47$content$47$10$45$3a$46$html from '../../client/data/content/10-3a.html'
+import client$47$data$47$content$47$10$45$3b$46$html from '../../client/data/content/10-3b.html'
+import client$47$data$47$content$47$10$45$3c$46$html from '../../client/data/content/10-3c.html'
+import client$47$data$47$content$47$10$45$3d$46$html from '../../client/data/content/10-3d.html'
+import client$47$data$47$content$47$10$45$3e$46$html from '../../client/data/content/10-3e.html'
+import client$47$data$47$content$47$10$45$3f$46$html from '../../client/data/content/10-3f.html'
+import client$47$data$47$content$47$10$45$3g$46$html from '../../client/data/content/10-3g.html'
+import client$47$data$47$content$47$10$45$3h$46$html from '../../client/data/content/10-3h.html'
+import client$47$data$47$content$47$11$45$1a$46$html from '../../client/data/content/11-1a.html'
+import client$47$data$47$content$47$11$45$1b$46$html from '../../client/data/content/11-1b.html'
+import client$47$data$47$content$47$11$45$2a$46$html from '../../client/data/content/11-2a.html'
+import client$47$data$47$content$47$11$45$2b$46$html from '../../client/data/content/11-2b.html'
+import client$47$data$47$content$47$11$45$2c$46$html from '../../client/data/content/11-2c.html'
+import client$47$data$47$content$47$11$45$2d$46$html from '../../client/data/content/11-2d.html'
+import client$47$data$47$content$47$11$45$2e$46$html from '../../client/data/content/11-2e.html'
+import client$47$data$47$content$47$11$45$2f$46$html from '../../client/data/content/11-2f.html'
+import client$47$data$47$content$47$11$45$2g$46$html from '../../client/data/content/11-2g.html'
+import client$47$data$47$content$47$11$45$2h$46$html from '../../client/data/content/11-2h.html'
+import client$47$data$47$content$47$1910$45$134$45$a$45$permissible$45$practice$46$html from '../../client/data/content/1910-134-a-permissible-practice.html'
+import client$47$data$47$content$47$1910$45$134b$45$definitions$46$html from '../../client/data/content/1910-134b-definitions.html'
+import client$47$data$47$content$47$1910$45$134c$45$respiratory$45$protection$45$program$46$html from '../../client/data/content/1910-134c-respiratory-protection-program.html'
+import client$47$data$47$content$47$1910$45$134d$45$selection$45$of$45$respirators$46$html from '../../client/data/content/1910-134d-selection-of-respirators.html'
+import client$47$data$47$content$47$1910$45$134e$45$medical$45$evaluation$46$html from '../../client/data/content/1910-134e-medical-evaluation.html'
+import client$47$data$47$content$47$1910$45$134f$45$fit$45$testing$46$html from '../../client/data/content/1910-134f-fit-testing.html'
+import client$47$data$47$content$47$1910$45$134g$45$use$45$of$45$respirators$46$html from '../../client/data/content/1910-134g-use-of-respirators.html'
+import client$47$data$47$content$47$1910$45$134h$45$maintenance$45$and$45$care$45$of$45$respirators$46$html from '../../client/data/content/1910-134h-maintenance-and-care-of-respirators.html'
+import client$47$data$47$content$47$1910$45$134i$45$breathing$45$air$45$quality$45$and$45$use$46$html from '../../client/data/content/1910-134i-breathing-air-quality-and-use.html'
+import client$47$data$47$content$47$1910$45$134j$45$identification$45$of$45$filters$45$cartridges$45$and$45$canisters$46$html from '../../client/data/content/1910-134j-identification-of-filters-cartridges-and-canisters.html'
+import client$47$data$47$content$47$1910$45$134k$45$training$45$and$45$information$46$html from '../../client/data/content/1910-134k-training-and-information.html'
+import client$47$data$47$content$47$1910$45$134l$45$program$45$evaluation$46$html from '../../client/data/content/1910-134l-program-evaluation.html'
+import client$47$data$47$content$47$1910$45$134m$45$recordkeeping$46$html from '../../client/data/content/1910-134m-recordkeeping.html'
+import client$47$data$47$content$47$2$46$html from '../../client/data/content/2.html'
+import client$47$data$47$content$47$3a$46$html from '../../client/data/content/3a.html'
+import client$47$data$47$content$47$3b$46$html from '../../client/data/content/3b.html'
+import client$47$data$47$content$47$556$46$html from '../../client/data/content/556.html'
+import client$47$data$47$content$47$5a$46$html from '../../client/data/content/5a.html'
+import client$47$data$47$content$47$5b$46$html from '../../client/data/content/5b.html'
+import client$47$data$47$content$47$7a$46$html from '../../client/data/content/7a.html'
+import client$47$data$47$content$47$7b$46$html from '../../client/data/content/7b.html'
+import client$47$data$47$content$47$7c$46$html from '../../client/data/content/7c.html'
+import client$47$data$47$content$47$7d$46$html from '../../client/data/content/7d.html'
+import client$47$data$47$content$47$7e$46$html from '../../client/data/content/7e.html'
+import client$47$data$47$content$47$7f$46$html from '../../client/data/content/7f.html'
+import client$47$data$47$content$47$9$45$1a$46$html from '../../client/data/content/9-1a.html'
+import client$47$data$47$content$47$9$45$1b$46$html from '../../client/data/content/9-1b.html'
+import client$47$data$47$content$47$9$45$3a$46$html from '../../client/data/content/9-3a.html'
+import client$47$data$47$content$47$9$45$3b$46$html from '../../client/data/content/9-3b.html'
+import client$47$data$47$content$47$9$45$3c$46$html from '../../client/data/content/9-3c.html'
+import client$47$data$47$content$47$9$45$3d$46$html from '../../client/data/content/9-3d.html'
+import client$47$data$47$content$47$9$45$3e$46$html from '../../client/data/content/9-3e.html'
+import client$47$data$47$content$47$9$45$3f$46$html from '../../client/data/content/9-3f.html'
+import client$47$data$47$content$47$9$45$3g$46$html from '../../client/data/content/9-3g.html'
+import client$47$data$47$content$47$9$45$3h$46$html from '../../client/data/content/9-3h.html'
+import client$47$data$47$content$47$acronyms$46$html from '../../client/data/content/acronyms.html'
+import client$47$data$47$content$47$appendix$45$a$45$fit$45$testing$45$procedures$45$mandatory$46$html from '../../client/data/content/appendix-a-fit-testing-procedures-mandatory.html'
+import client$47$data$47$content$47$appendix$45$b$45$1$45$user$45$seal$45$check$45$procedures$45$mandatory$46$html from '../../client/data/content/appendix-b-1-user-seal-check-procedures-mandatory.html'
+import client$47$data$47$content$47$appendix$45$c$45$osha$45$respirator$45$medical$45$evaluation$45$questionnaire$45$mandatory$46$html from '../../client/data/content/appendix-c-osha-respirator-medical-evaluation-questionnaire-mandatory.html'
+import client$47$data$47$content$47$appendix$45$d$45$mandatory$45$information$45$for$45$employees$45$using$45$respirators$45$when$45$not$45$required$45$under$45$the$45$standard$46$html from '../../client/data/content/appendix-d-mandatory-information-for-employees-using-respirators-when-not-required-under-the-standard.html'
+import client$47$data$47$content$47$before$45$you$45$start$46$html from '../../client/data/content/before-you-start.html'
+import client$47$data$47$content$47$caring$45$for$45$your$45$respirator$46$html from '../../client/data/content/caring-for-your-respirator.html'
+import client$47$data$47$content$47$emma$45$retest__trashed$46$html from '../../client/data/content/emma-retest__trashed.html'
+import client$47$data$47$content$47$employer$45$responsibilites$46$html from '../../client/data/content/employer-responsibilites.html'
+import client$47$data$47$content$47$fit$45$testing$46$html from '../../client/data/content/fit-testing.html'
+import client$47$data$47$content$47$glossary$46$html from '../../client/data/content/glossary.html'
+import client$47$data$47$content$47$noah$45$test__trashed$46$html from '../../client/data/content/noah-test__trashed.html'
+import client$47$data$47$content$47$osha$45$standards$46$html from '../../client/data/content/osha-standards.html'
+import client$47$data$47$content$47$osha$45$standards__trashed$46$html from '../../client/data/content/osha-standards__trashed.html'
+import client$47$data$47$content$47$purpose$45$of$45$respirators$46$html from '../../client/data/content/purpose-of-respirators.html'
+import client$47$data$47$content$47$resources$46$html from '../../client/data/content/resources.html'
+import client$47$data$47$content$47$respirator$45$characteristics$46$html from '../../client/data/content/respirator-characteristics.html'
+import client$47$data$47$content$47$respirator$45$safety$46$html from '../../client/data/content/respirator-safety.html'
+import client$47$data$47$content$47$text$45$test__trashed$46$html from '../../client/data/content/text-test__trashed.html'
+import client$47$data$47$content$47$top$45$10$45$terms$45$to$45$know$46$html from '../../client/data/content/top-10-terms-to-know.html'
+import client$47$data$47$content$47$types$45$of$45$respirators$46$html from '../../client/data/content/types-of-respirators.html'
+import client$47$data$47$content$47$using$45$this$45$app$46$html from '../../client/data/content/using-this-app.html'
+import client$47$data$47$content$47$who$45$should$45$use$45$it$46$html from '../../client/data/content/who-should-use-it.html'
+
+export default [
+	{ path: 'client/data/content/1.html', export: client$47$data$47$content$47$1$46$html },
+	{ path: 'client/data/content/10-1a.html', export: client$47$data$47$content$47$10$45$1a$46$html },
+	{ path: 'client/data/content/10-1b.html', export: client$47$data$47$content$47$10$45$1b$46$html },
+	{ path: 'client/data/content/10-3a.html', export: client$47$data$47$content$47$10$45$3a$46$html },
+	{ path: 'client/data/content/10-3b.html', export: client$47$data$47$content$47$10$45$3b$46$html },
+	{ path: 'client/data/content/10-3c.html', export: client$47$data$47$content$47$10$45$3c$46$html },
+	{ path: 'client/data/content/10-3d.html', export: client$47$data$47$content$47$10$45$3d$46$html },
+	{ path: 'client/data/content/10-3e.html', export: client$47$data$47$content$47$10$45$3e$46$html },
+	{ path: 'client/data/content/10-3f.html', export: client$47$data$47$content$47$10$45$3f$46$html },
+	{ path: 'client/data/content/10-3g.html', export: client$47$data$47$content$47$10$45$3g$46$html },
+	{ path: 'client/data/content/10-3h.html', export: client$47$data$47$content$47$10$45$3h$46$html },
+	{ path: 'client/data/content/11-1a.html', export: client$47$data$47$content$47$11$45$1a$46$html },
+	{ path: 'client/data/content/11-1b.html', export: client$47$data$47$content$47$11$45$1b$46$html },
+	{ path: 'client/data/content/11-2a.html', export: client$47$data$47$content$47$11$45$2a$46$html },
+	{ path: 'client/data/content/11-2b.html', export: client$47$data$47$content$47$11$45$2b$46$html },
+	{ path: 'client/data/content/11-2c.html', export: client$47$data$47$content$47$11$45$2c$46$html },
+	{ path: 'client/data/content/11-2d.html', export: client$47$data$47$content$47$11$45$2d$46$html },
+	{ path: 'client/data/content/11-2e.html', export: client$47$data$47$content$47$11$45$2e$46$html },
+	{ path: 'client/data/content/11-2f.html', export: client$47$data$47$content$47$11$45$2f$46$html },
+	{ path: 'client/data/content/11-2g.html', export: client$47$data$47$content$47$11$45$2g$46$html },
+	{ path: 'client/data/content/11-2h.html', export: client$47$data$47$content$47$11$45$2h$46$html },
+	{ path: 'client/data/content/1910-134-a-permissible-practice.html', export: client$47$data$47$content$47$1910$45$134$45$a$45$permissible$45$practice$46$html },
+	{ path: 'client/data/content/1910-134b-definitions.html', export: client$47$data$47$content$47$1910$45$134b$45$definitions$46$html },
+	{ path: 'client/data/content/1910-134c-respiratory-protection-program.html', export: client$47$data$47$content$47$1910$45$134c$45$respiratory$45$protection$45$program$46$html },
+	{ path: 'client/data/content/1910-134d-selection-of-respirators.html', export: client$47$data$47$content$47$1910$45$134d$45$selection$45$of$45$respirators$46$html },
+	{ path: 'client/data/content/1910-134e-medical-evaluation.html', export: client$47$data$47$content$47$1910$45$134e$45$medical$45$evaluation$46$html },
+	{ path: 'client/data/content/1910-134f-fit-testing.html', export: client$47$data$47$content$47$1910$45$134f$45$fit$45$testing$46$html },
+	{ path: 'client/data/content/1910-134g-use-of-respirators.html', export: client$47$data$47$content$47$1910$45$134g$45$use$45$of$45$respirators$46$html },
+	{ path: 'client/data/content/1910-134h-maintenance-and-care-of-respirators.html', export: client$47$data$47$content$47$1910$45$134h$45$maintenance$45$and$45$care$45$of$45$respirators$46$html },
+	{ path: 'client/data/content/1910-134i-breathing-air-quality-and-use.html', export: client$47$data$47$content$47$1910$45$134i$45$breathing$45$air$45$quality$45$and$45$use$46$html },
+	{ path: 'client/data/content/1910-134j-identification-of-filters-cartridges-and-canisters.html', export: client$47$data$47$content$47$1910$45$134j$45$identification$45$of$45$filters$45$cartridges$45$and$45$canisters$46$html },
+	{ path: 'client/data/content/1910-134k-training-and-information.html', export: client$47$data$47$content$47$1910$45$134k$45$training$45$and$45$information$46$html },
+	{ path: 'client/data/content/1910-134l-program-evaluation.html', export: client$47$data$47$content$47$1910$45$134l$45$program$45$evaluation$46$html },
+	{ path: 'client/data/content/1910-134m-recordkeeping.html', export: client$47$data$47$content$47$1910$45$134m$45$recordkeeping$46$html },
+	{ path: 'client/data/content/2.html', export: client$47$data$47$content$47$2$46$html },
+	{ path: 'client/data/content/3a.html', export: client$47$data$47$content$47$3a$46$html },
+	{ path: 'client/data/content/3b.html', export: client$47$data$47$content$47$3b$46$html },
+	{ path: 'client/data/content/556.html', export: client$47$data$47$content$47$556$46$html },
+	{ path: 'client/data/content/5a.html', export: client$47$data$47$content$47$5a$46$html },
+	{ path: 'client/data/content/5b.html', export: client$47$data$47$content$47$5b$46$html },
+	{ path: 'client/data/content/7a.html', export: client$47$data$47$content$47$7a$46$html },
+	{ path: 'client/data/content/7b.html', export: client$47$data$47$content$47$7b$46$html },
+	{ path: 'client/data/content/7c.html', export: client$47$data$47$content$47$7c$46$html },
+	{ path: 'client/data/content/7d.html', export: client$47$data$47$content$47$7d$46$html },
+	{ path: 'client/data/content/7e.html', export: client$47$data$47$content$47$7e$46$html },
+	{ path: 'client/data/content/7f.html', export: client$47$data$47$content$47$7f$46$html },
+	{ path: 'client/data/content/9-1a.html', export: client$47$data$47$content$47$9$45$1a$46$html },
+	{ path: 'client/data/content/9-1b.html', export: client$47$data$47$content$47$9$45$1b$46$html },
+	{ path: 'client/data/content/9-3a.html', export: client$47$data$47$content$47$9$45$3a$46$html },
+	{ path: 'client/data/content/9-3b.html', export: client$47$data$47$content$47$9$45$3b$46$html },
+	{ path: 'client/data/content/9-3c.html', export: client$47$data$47$content$47$9$45$3c$46$html },
+	{ path: 'client/data/content/9-3d.html', export: client$47$data$47$content$47$9$45$3d$46$html },
+	{ path: 'client/data/content/9-3e.html', export: client$47$data$47$content$47$9$45$3e$46$html },
+	{ path: 'client/data/content/9-3f.html', export: client$47$data$47$content$47$9$45$3f$46$html },
+	{ path: 'client/data/content/9-3g.html', export: client$47$data$47$content$47$9$45$3g$46$html },
+	{ path: 'client/data/content/9-3h.html', export: client$47$data$47$content$47$9$45$3h$46$html },
+	{ path: 'client/data/content/acronyms.html', export: client$47$data$47$content$47$acronyms$46$html },
+	{ path: 'client/data/content/appendix-a-fit-testing-procedures-mandatory.html', export: client$47$data$47$content$47$appendix$45$a$45$fit$45$testing$45$procedures$45$mandatory$46$html },
+	{ path: 'client/data/content/appendix-b-1-user-seal-check-procedures-mandatory.html', export: client$47$data$47$content$47$appendix$45$b$45$1$45$user$45$seal$45$check$45$procedures$45$mandatory$46$html },
+	{ path: 'client/data/content/appendix-c-osha-respirator-medical-evaluation-questionnaire-mandatory.html', export: client$47$data$47$content$47$appendix$45$c$45$osha$45$respirator$45$medical$45$evaluation$45$questionnaire$45$mandatory$46$html },
+	{ path: 'client/data/content/appendix-d-mandatory-information-for-employees-using-respirators-when-not-required-under-the-standard.html', export: client$47$data$47$content$47$appendix$45$d$45$mandatory$45$information$45$for$45$employees$45$using$45$respirators$45$when$45$not$45$required$45$under$45$the$45$standard$46$html },
+	{ path: 'client/data/content/before-you-start.html', export: client$47$data$47$content$47$before$45$you$45$start$46$html },
+	{ path: 'client/data/content/caring-for-your-respirator.html', export: client$47$data$47$content$47$caring$45$for$45$your$45$respirator$46$html },
+	{ path: 'client/data/content/emma-retest__trashed.html', export: client$47$data$47$content$47$emma$45$retest__trashed$46$html },
+	{ path: 'client/data/content/employer-responsibilites.html', export: client$47$data$47$content$47$employer$45$responsibilites$46$html },
+	{ path: 'client/data/content/fit-testing.html', export: client$47$data$47$content$47$fit$45$testing$46$html },
+	{ path: 'client/data/content/glossary.html', export: client$47$data$47$content$47$glossary$46$html },
+	{ path: 'client/data/content/noah-test__trashed.html', export: client$47$data$47$content$47$noah$45$test__trashed$46$html },
+	{ path: 'client/data/content/osha-standards.html', export: client$47$data$47$content$47$osha$45$standards$46$html },
+	{ path: 'client/data/content/osha-standards__trashed.html', export: client$47$data$47$content$47$osha$45$standards__trashed$46$html },
+	{ path: 'client/data/content/purpose-of-respirators.html', export: client$47$data$47$content$47$purpose$45$of$45$respirators$46$html },
+	{ path: 'client/data/content/resources.html', export: client$47$data$47$content$47$resources$46$html },
+	{ path: 'client/data/content/respirator-characteristics.html', export: client$47$data$47$content$47$respirator$45$characteristics$46$html },
+	{ path: 'client/data/content/respirator-safety.html', export: client$47$data$47$content$47$respirator$45$safety$46$html },
+	{ path: 'client/data/content/text-test__trashed.html', export: client$47$data$47$content$47$text$45$test__trashed$46$html },
+	{ path: 'client/data/content/top-10-terms-to-know.html', export: client$47$data$47$content$47$top$45$10$45$terms$45$to$45$know$46$html },
+	{ path: 'client/data/content/types-of-respirators.html', export: client$47$data$47$content$47$types$45$of$45$respirators$46$html },
+	{ path: 'client/data/content/using-this-app.html', export: client$47$data$47$content$47$using$45$this$45$app$46$html },
+	{ path: 'client/data/content/who-should-use-it.html', export: client$47$data$47$content$47$who$45$should$45$use$45$it$46$html }
+]
---
+++ client/data/globbed-routes.js
@@ -0,0 +1,41 @@
+import client$47$routes$47$app$46$js from '../../client/routes/app.js'
+import client$47$routes$47$calculate$47$calculate$46$js from '../../client/routes/calculate/calculate.js'
+import client$47$routes$47$category$47$category$46$js from '../../client/routes/category/category.js'
+import client$47$routes$47$chemicals$47$chemicals$46$js from '../../client/routes/chemicals/chemicals.js'
+import client$47$routes$47$contact$47$contact$46$js from '../../client/routes/contact/contact.js'
+import client$47$routes$47$content$47$content$46$js from '../../client/routes/content/content.js'
+import client$47$routes$47$home$47$home$46$js from '../../client/routes/home/home.js'
+import client$47$routes$47$info$47$info$46$js from '../../client/routes/info/info.js'
+import client$47$routes$47$not$45$found$47$not$45$found$46$js from '../../client/routes/not-found/not-found.js'
+import client$47$routes$47$options$45$list$47$content$46$js from '../../client/routes/options-list/content.js'
+import client$47$routes$47$respirator$45$picker$47$respirator$45$introduction$46$js from '../../client/routes/respirator-picker/respirator-introduction.js'
+import client$47$routes$47$respirator$45$picker$47$respirator$45$picker$46$js from '../../client/routes/respirator-picker/respirator-picker.js'
+import client$47$routes$47$calculate$47$hr$47$hr$46$js from '../../client/routes/calculate/hr/hr.js'
+import client$47$routes$47$calculate$47$mcmuc$47$generate$45$table$46$js from '../../client/routes/calculate/mcmuc/generate-table.js'
+import client$47$routes$47$calculate$47$mcmuc$47$mcmuc$46$js from '../../client/routes/calculate/mcmuc/mcmuc.js'
+import client$47$routes$47$calculate$47$muc$47$muc$46$js from '../../client/routes/calculate/muc/muc.js'
+import client$47$routes$47$calculate$47$twa$47$generate$45$table$46$js from '../../client/routes/calculate/twa/generate-table.js'
+import client$47$routes$47$calculate$47$twa$47$twa$46$js from '../../client/routes/calculate/twa/twa.js'
+import client$47$routes$47$category$47$content$47$content$46$js from '../../client/routes/category/content/content.js'
+
+export default [
+	client$47$routes$47$app$46$js,
+	client$47$routes$47$calculate$47$calculate$46$js,
+	client$47$routes$47$category$47$category$46$js,
+	client$47$routes$47$chemicals$47$chemicals$46$js,
+	client$47$routes$47$contact$47$contact$46$js,
+	client$47$routes$47$content$47$content$46$js,
+	client$47$routes$47$home$47$home$46$js,
+	client$47$routes$47$info$47$info$46$js,
+	client$47$routes$47$not$45$found$47$not$45$found$46$js,
+	client$47$routes$47$options$45$list$47$content$46$js,
+	client$47$routes$47$respirator$45$picker$47$respirator$45$introduction$46$js,
+	client$47$routes$47$respirator$45$picker$47$respirator$45$picker$46$js,
+	client$47$routes$47$calculate$47$hr$47$hr$46$js,
+	client$47$routes$47$calculate$47$mcmuc$47$generate$45$table$46$js,
+	client$47$routes$47$calculate$47$mcmuc$47$mcmuc$46$js,
+	client$47$routes$47$calculate$47$muc$47$muc$46$js,
+	client$47$routes$47$calculate$47$twa$47$generate$45$table$46$js,
+	client$47$routes$47$calculate$47$twa$47$twa$46$js,
+	client$47$routes$47$category$47$content$47$content$46$js
+]
