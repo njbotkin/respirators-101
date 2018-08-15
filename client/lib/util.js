2819bc9b10a23cae67448af365cb16cc12890073
Noah <njbotkin@gmail.com>
2018-08-15 13:26:08 -0500

    add fake decimal to integers

--- client/lib/util.js
+++ client/lib/util.js
@@ -1,8 +1,16 @@
-// rounds and adds commas
+// rounds and adds commas, add fake decimal to imply precision
 export function round(v) {
 	if(typeof v === 'undefined' || !number(v)) return 0
 	v = String(Math.round(v * 100) / 100) 
-	let decimal = v.indexOf('.') == -1 ? v.length : v.indexOf('.')
+
+	let decimal
+	if(v.indexOf('.') == -1) {
+		v += '.0'
+		decimal = v.length - 2
+	} else {
+		decimal = v.indexOf('.')
+	}
+
 	for(let i = decimal-3; i > 0; i-=3) {
 		v = v.substr(0, i) + ',' + v.substr(i)
 	}
